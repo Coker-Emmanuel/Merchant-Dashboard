b0daf37ec4abbf1838190418117bb18d8716321b
Coker-Emmanuel <mrkoker35@gmail.com>
2023-11-27 19:42:03 +0100

    Commit Update

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 let profileMenu = document.getElementById("profileMenu");
+let alertMenu = document.getElementById("alertMenu");
 
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
+    if (alertMenu.classList[1] == "open-menu"){
+        alertMenu.classList.toggle("open-menu");
+    }
+}
+
+function toggleAlert(){
+    alertMenu.classList.toggle("open-menu");
+    if (profileMenu.classList[1] == "open-menu"){
+        profileMenu.classList.toggle("open-menu");
+    }
 }
